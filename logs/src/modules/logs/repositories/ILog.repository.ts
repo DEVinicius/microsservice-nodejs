0f042615ef5e4ba6edfeec458b09844838d5f793
Vinicius Pereira de Oliveira <vinicius.oliveira@actdigital.com.br>
2021-07-13 22:04:15 -0300

    [NEW] Log Repository

--- logs/src/modules/logs/repositories/ILog.repository.ts
+++ logs/src/modules/logs/repositories/ILog.repository.ts
@@ -1,3 +1,6 @@
+import { Log } from './../entity/log.entity';
+import { ICreateLogDto } from "./dto/ICreateLog.dto";
+
 export interface ILogRepository {
-    
+    create(createLogDto:ICreateLogDto):Promise<Log>;
 }
--- logs/src/modules/logs/repositories/implementation/Log.repository.ts
+++ logs/src/modules/logs/repositories/implementation/Log.repository.ts
@@ -1,5 +1,20 @@
+import { Log } from './../../entity/log.entity';
+import { ICreateLogDto } from "../dto/ICreateLog.dto";
 import { ILogRepository } from "../ILog.repository";
 
 export class LogRepository implements ILogRepository {
-    
+    private logRepository:Log[];
+
+    constructor() {
+        this.logRepository = [];
+    }
+
+    async create(createLogDto: ICreateLogDto): Promise<Log> {
+        const log = new Log();
+
+        Object.assign(log, createLogDto);
+        this.logRepository.push(log);
+
+        return log;
+    }
 }
